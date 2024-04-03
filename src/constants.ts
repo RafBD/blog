f7c17c34079a2d6b1edaadbbb8e1d830cc0bb5f3
RaffaBD <rafa.bediaz@gmail.com>
2024-04-02 19:19:33 -0600

    Add constants file

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+export const SITE_TITLE = "BlogName";
+export const SITE_DESCRIPTION =
+  "Blog de Luis Alberto Benitez Diaz. Periodista en El Economista";
+export const HOMEPAGE_ARTICLE_LIMIT = 7;
+export const ARTICLES_PER_PAGE = 6;
